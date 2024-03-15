41c54d0fc2e29b03aeaef72b6d16ac49e4bd77dd
Kapil Duwadi <Kapil.Duwadi@nrel.gov>
2024-03-15 16:12:26 -0500

    cosmetic modifications

--- app/empty-entity-view.tsx
+++ app/empty-entity-view.tsx
@@ -16,8 +16,8 @@ export const EmptyView: React.FC<EmptyViewProps> = ({
                 <IconComponent size={100} className="text-gray-500" />
             </div>
             <p className="my-10 text-gray-400
-                px-5 py-1 rounded-md"> No {name} have been added. First add your license by
-                clicking add new on left pane. </p>
+                px-5 py-1 rounded-md"> No {name} have been added. Add your first entry by
+                clicking <span className="bg-gray-200 rounded-md px-1 mr-1"> Add New</span> on left pane. </p>
         </div>
     )
 }
